2a14cb146e2ad3891b9f6eae8fccd27ba824bf40
shirogaku <shirogaku@gmail.com>
2016-12-10 14:36:40 +0700

    Init run OTA first

--- init.lua
+++ init.lua
@@ -2,9 +2,15 @@ station_cfg = {}
 station_cfg.ssid = ""
 station_cfg.pwd = ""
 station_cfg.bssid = ""
-startupmode = "skip"
+startupmode = "OTA"
 OTA_version_link = "http://foo.bar/version.html"
 OTA_startup_link = "http://foo.bar/startup.lua"
+
+function launchstartup()
+	wifi.sta.disconnect()
+	collectgarbage()
+	dofile("startup.lua")
+end
 
 function startup()
     if startupmode == "abort" then
@@ -39,51 +45,50 @@ function startup()
         
         wifi.setmode(wifi.STATION)
         wifi.sta.config(station_cfg)
-        
+		
 		is_need_update = false
-		is_version_check_done = false
-		-- HTTP get is asynchronous need to fix this
-        http.get(OTA_version_link, nil, function(code, data)
-            if code == 200 and data ~= installed_version then
-                download_version = data
-                print("New version found. Updating")
-				is_need_update = true
-				is_version_check_done = true
-            elseif data == installed_version then
-                print("Current version is lastest. Restarting")
-                node.restart()
-            else
-                print("Version file not found. Restarting")
-                node.restart()
-            end
-        end
-        )
 		
 		tmr.alarm(1, 2000, tmr.ALARM_SINGLE, function()
+			http.get(OTA_version_link, nil, function(code, data)
+				if code == 200 and data ~= installed_version then
+					download_version = data
+					print("New version found. Updating")
+					is_need_update = true
+				elseif data == installed_version then
+					print("Installed version is lastest. Running startup file.")
+					launchstartup()
+				else
+					print("Version file not found. Running startup file")
+					launchstartup()
+				end
+			end
+			)	
+		end)
+		
+		tmr.alarm(2, 5000, tmr.ALARM_SINGLE, function()
 		    if is_need_update then
-                print("Here!!!")
                 http.get(OTA_startup_link, nil, function(code, data)
-                if code == 200 and data ~= nil then
-                    file.open("startup.lua", "w")
-                    file.write(data)
-                    file.close()
-                    file.open("version.txt", "w")
-                    file.write(download_version)
-                    file.close()
-                    print("Update completed. Restarting")
-                else
-                    print("Couldn't Download startup file. Restarting")
-                end
-                    node.restart()
+					if code == 200 and data ~= nil then
+						file.open("startup.lua", "w")
+						file.write(data)
+						file.close()
+						file.open("version.txt", "w")
+						file.write(download_version)
+						file.close()
+						print("Update completed. Running startup file.")
+						launchstartup()
+					else
+						print("Couldn't Download startup file. Restarting.")
+						launchstartup()
+					end
                 end
                 )
             end
 		end)
-        print("Outside update condition")
     end
 end
 
-print("Luanching startup script in 5 seconds.")
-print("Type startupmode=\"abort\" for abort")
-print("Type startupmode=\"OTA\" for running OTA and run startup script.")
+print("Launching OTA in 5 seconds.")
+print("Type startupmode=\"abort\" for abort.")
+print("Type startupmode=\"skip\" for skipping OTA.")
 tmr.alarm(0, 5000,tmr.ALARM_SINGLE, startup)
